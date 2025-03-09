f4bed30625def775382f6f8e68d8d355830db045
Vinicius Krüger <krugerdv12@gmail.com>
2025-03-09 11:35:09 -0300

    aulas e exercicios

---
+++ modelo-tempo/script.js
@@ -0,0 +1,24 @@
+var hora = new Date()
+var horario = hora.getHours()
+var texto = window.document.querySelector('div.hora')
+
+var horario = 13
+
+texto.innerHTML = `Agora sao ${horario} horas`
+
+if (horario >= 4 && horario <= 12){
+     document.querySelector("div.img").style.backgroundImage = 'url(imagens/manha.jpg)'
+    document.body.style.background = '#EADBCB'
+   
+}
+else if(horario < 18 && horario > 12){
+    document.querySelector("div.img").style.backgroundImage = 'url(imagens/tarde.jpg)'
+     document.body.style.background = '#773E33'
+    
+}
+
+else{
+     document.querySelector("div.img").style.backgroundImage = 'url(imagens/noite.png)'
+     document.body.style.background = '#616267'
+   
+}
